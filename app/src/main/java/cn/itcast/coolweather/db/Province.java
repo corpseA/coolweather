2efba4c6c85fda80b2a1bf0c44136094de7ca4aa
coolweather <760734506@qq.com>
2019-05-17 09:06:19 +0800

    加入创建数据库和表的各项配置

---
+++ app/src/main/java/cn/itcast/coolweather/db/Province.java
@@ -0,0 +1,39 @@
+package cn.itcast.coolweather.db;
+
+/**
+ * Created by J on 2019/5/17.
+ */
+import org.litepal.crud.DataSupport;
+
+public class Province extends DataSupport {
+
+    private int id;
+
+    private String provinceName;
+
+    private int provinceCode;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getProvinceName() {
+        return provinceName;
+    }
+
+    public void setProvinceName(String provinceName) {
+        this.provinceName = provinceName;
+    }
+
+    public int getProvinceCode() {
+        return provinceCode;
+    }
+
+    public void setProvinceCode(int provinceCode) {
+        this.provinceCode = provinceCode;
+    }
+}
